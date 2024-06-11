6fdbca4bf9620f6efd026c5c842e23a28cfe8922
Anton Karpenko <karpolan@karpolan.com>
2024-06-11 18:55:53 +0200

    fix: envRequired(process.env.NEXT_PUBLIC_PUBLIC_URL) is commented

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,8 @@ export const IS_DEBUG = process.env.NEXT_PUBLIC_DEBUG === 'true'; // Enables log
 
 export const IS_PRODUCTION = getCurrentEnvironment() === 'production'; // Enables analytics, etc.
 
-export const PUBLIC_URL = envRequired(process.env.NEXT_PUBLIC_PUBLIC_URL);
+// export const PUBLIC_URL = envRequired(process.env.NEXT_PUBLIC_PUBLIC_URL); // Variant 1: .env variable is required
+export const PUBLIC_URL = process.env.NEXT_PUBLIC_PUBLIC_URL; // Variant 2: .env variable is optional
 
 IS_DEBUG &&
   console.log('@/config', {
